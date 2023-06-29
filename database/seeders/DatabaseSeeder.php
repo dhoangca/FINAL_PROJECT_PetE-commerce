7e8148fb977fffd51a3c5ef1f4cae52a6ef24e4a
Hoang Ca <accclonek25@gmail.com>
2023-06-29 12:20:15 +0700

    loi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(SlidesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
-        $this->call(PetsTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
         $this->call(PaymentMethodsTableSeeder::class);
         $this->call(OrdersTableSeeder::class);
         $this->call(OrderItemsTableSeeder::class);
