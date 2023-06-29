d165615d5a285e8919deee42563f1dbf123dcc99
Hoang Ca <accclonek25@gmail.com>
2023-06-29 13:05:54 +0700

    loi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         // Call your individual seeders here
         $this->call(UsersTableSeeder::class);
-        $this->call(SlidesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(PaymentMethodsTableSeeder::class);
-        $this->call(OrdersTableSeeder::class);
-        $this->call(OrderItemsTableSeeder::class);
-        $this->call(CartTableSeeder::class);
+        $this->call(StatusTableSeeder::class);
     }
 }
